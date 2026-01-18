579643fcaaeb31360160546a3b5a2455b06be685
AaronWong27 <aaronwong2702@gmail.com>
2026-01-17 19:51:59 -0800

    lol

--- mobile/config/firebase.js
+++ mobile/config/firebase.js
@@ -6,13 +6,14 @@ import { getAuth } from 'firebase/auth';
 
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "API_KEY_HERE",
-    authDomain: "scensor-a620a.firebaseapp.com",
-    databaseURL: "https://scensor-a620a-default-rtdb.firebaseio.com",
-    projectId: "scensor-a620a",
-    storageBucket: "scensor-a620a.firebasestorage.app",
-    messagingSenderId: "353469780357",
-    appId: "1:353469780357:android:e11b8f6d48f78d860334ec"
+  apiKey: "AIzaSyDtHJfENFfuagPlaiXbpCdFmgZ-KtLzYqc",
+  authDomain: "scensor-28457.firebaseapp.com",
+  databaseURL: "https://scensor-28457-default-rtdb.firebaseio.com",
+  projectId: "scensor-28457",
+  storageBucket: "scensor-28457.firebasestorage.app",
+  messagingSenderId: "245035409975",
+  appId: "1:245035409975:web:32876b234428c22aef55b2",
+  measurementId: "G-2KDPPME9H4"
 };
 
 // Initialize Firebase
